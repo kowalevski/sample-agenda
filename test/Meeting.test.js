334b451ad7c0eb67138d8f6bc8da8934782f7ab9
Maxim Kovalevsky <Maxim.Kovalevsky@nordigy.ru>
2019-12-05 21:52:35 +0300

    creates new test for rendering team name and added support husky and eslint for git hooks

--- test/Meeting.test.js
+++ test/Meeting.test.js
@@ -1,1 +1,7 @@
-describe("Meeting", () => {});
+describe("Meeting", () => {
+  it("renders the team name", () => {
+    // const component = null;
+    // const container = null;
+    expect(document.body.textContent).toMatch("Team A");
+  });
+});
