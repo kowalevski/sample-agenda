2de089542ece326b0c8e2bff53f9017a71ab0548
Maxim Kovalevsky <Maxim.Kovalevsky@nordigy.ru>
2019-12-06 23:14:22 +0300

    refactored file with Meetings tests

--- test/Meeting.test.jsx
+++ test/Meeting.test.jsx
@@ -10,23 +10,18 @@ describe("Meeting", () => {
     container = document.createElement("div");
   });
 
+  // basically just a simple emulation of Enzyme's work here
   const render = component => ReactDOM.render(component, container);
 
   it("renders the team name", () => {
     team = { name: "Team A" };
-
-    // basically just a simple emulation of Enzyme's work here
     render(<Meeting team={team} />);
-
     expect(container.textContent).toMatch("Team A");
   });
   // triangulation
   it("renders another team name", () => {
     team = { name: "Team B" };
-    container = document.createElement("div");
-
     render(<Meeting team={team} />);
-
     expect(container.textContent).toMatch("Team B");
   });
 });
