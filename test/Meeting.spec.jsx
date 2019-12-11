b6c92dca5add614b4539f72bfca136d441e2c507
mkl0g <ducktypings@gmail.com>
2019-12-11 22:13:33 +0300

    changed tests - used enzyme library instead react-dom

--- test/Meeting.spec.jsx
+++ test/Meeting.spec.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import { Meeting } from '../src/Meeting';
 
 describe('Meeting', () => {
   let container;
   let team;
 
-  beforeEach(() => {
-    container = document.createElement('div');
-  });
-
-  // basically just a simple emulation of Enzyme's work here
-  const render = component => ReactDOM.render(component, container);
-
   it('renders the team name', () => {
     team = { name: 'Team A' };
-    render(<Meeting team={team} />);
-    expect(container.textContent).toMatch('Team A');
+    container = shallow(<Meeting team={team} />);
+    expect(container.text()).toMatch('Team A');
   });
   // triangulation
   it('renders another team name', () => {
     team = { name: 'Team B' };
-    render(<Meeting team={team} />);
-    expect(container.textContent).toMatch('Team B');
+    container = shallow(<Meeting team={team} />);
+    expect(container.text()).toMatch('Team B');
   });
 });
--- test/MeetingsDayView.spec.jsx
+++ test/MeetingsDayView.spec.jsx
@@ -1,81 +1,87 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactTestUtils from 'react-dom/test-utils';
+import { shallow } from 'enzyme';
 import {
   MeetingsDayView,
   noMeetingsForTodayText
 } from '../src/MeetingsDayView';
 
 describe('MeetingsDayView', () => {
-  let container;
-  const today = new Date();
-  const meetings = [
-    {
-      startsAt: today.setHours(11, 0),
-      team: {
-        name: 'A'
+  describe('has empty list of meetings', () => {
+    let container;
+
+    beforeAll(() => {
+      container = shallow(<MeetingsDayView meetings={[]} />);
+    });
+
+    it('renders a div element with right id', () => {
+      expect(container.find('div#meetingsDayView')).not.toBeNull();
+    });
+    it('renders message about that there are no meetings today', () => {
+      expect(container.text()).toMatch(noMeetingsForTodayText);
+    });
+  });
+
+  describe('has list of two meetings', () => {
+    let container;
+    const today = new Date();
+    const meetings = [
+      {
+        startsAt: today.setHours(11, 0),
+        team: {
+          name: 'A'
+        }
+      },
+      {
+        startsAt: today.setHours(12, 0),
+        team: {
+          name: 'B'
+        }
       }
-    },
-    {
-      startsAt: today.setHours(12, 0),
-      team: {
-        name: 'B'
-      }
-    }
-  ];
-  const FIRST_MEETING_INDEX = 0;
-  const SECOND_MEETING_INDEX = 1;
+    ];
+    const FIRST_MEETING_INDEX = 0;
+    const SECOND_MEETING_INDEX = 1;
 
-  beforeEach(() => {
-    container = document.createElement('div');
-  });
+    beforeAll(() => {
+      container = shallow(<MeetingsDayView meetings={meetings} />);
+    });
 
-  const render = component => ReactDOM.render(component, container);
-
-  it('renders a div element with right id', () => {
-    render(<MeetingsDayView meetings={[]} />);
-    expect(container.querySelector('div#meetingsDayView')).not.toBeNull();
-  });
-  it('renders message about that there are no meetings today', () => {
-    render(<MeetingsDayView meetings={[]} />);
-    expect(container.textContent).toMatch(noMeetingsForTodayText);
-  });
-  it('selects the first meeting by default', () => {
-    render(<MeetingsDayView meetings={meetings} />);
-    expect(container.textContent).toMatch(
-      meetings[FIRST_MEETING_INDEX].team.name
-    );
-  });
-  it('renders few meetings in a ol element', () => {
-    render(<MeetingsDayView meetings={meetings} />);
-    expect(container.querySelector('ol')).not.toBeNull();
-    expect(container.querySelector('ol').children).toHaveLength(
-      meetings.length
-    );
-  });
-  it('renders each meeting in on li', () => {
-    render(<MeetingsDayView meetings={meetings} />);
-    expect(container.querySelectorAll('li')).toHaveLength(meetings.length);
-    expect(
-      container.querySelectorAll('li')[FIRST_MEETING_INDEX].textContent
-    ).toEqual('11:00');
-    expect(
-      container.querySelectorAll('li')[SECOND_MEETING_INDEX].textContent
-    ).toEqual('12:00');
-  });
-  it('renders a button element in each li', () => {
-    render(<MeetingsDayView meetings={meetings} />);
-    expect(container.querySelectorAll('li > button')).toHaveLength(2);
-    expect(
-      container.querySelectorAll('li > button')[FIRST_MEETING_INDEX].type
-    ).toEqual('button');
-  });
-  it('renders another meeting when selected', () => {
-    render(<MeetingsDayView meetings={meetings} />);
-    const button = container.querySelectorAll('button')[SECOND_MEETING_INDEX];
-    ReactTestUtils.Simulate.click(button);
-    expect(container.textContent).toMatch(
-      meetings[SECOND_MEETING_INDEX].team.name
-    );
+    it('selects the first meeting by default', () => {
+      expect(container.html()).toMatch(meetings[FIRST_MEETING_INDEX].team.name);
+    });
+    it('renders few meetings in a ol element', () => {
+      expect(container.find('ol')).not.toBeNull();
+      expect(container.find('ol').children()).toHaveLength(meetings.length);
+    });
+    it('renders each meeting in li', () => {
+      expect(container.find('li')).toHaveLength(meetings.length);
+      expect(
+        container
+          .find('li')
+          .first()
+          .text()
+      ).toEqual('11:00');
+      expect(
+        container
+          .find('li')
+          .at(SECOND_MEETING_INDEX)
+          .text()
+      ).toEqual('12:00');
+    });
+    it('renders a button element in each li', () => {
+      expect(container.find('li > button')).toHaveLength(2);
+      expect(
+        container
+          .find('li > button')
+          .first()
+          .type()
+      ).toEqual('button');
+    });
+    it('renders another meeting when selected', () => {
+      const button = container.find('button').at(SECOND_MEETING_INDEX);
+      button.simulate('click');
+      expect(container.html()).toMatch(
+        meetings[SECOND_MEETING_INDEX].team.name
+      );
+    });
   });
 });
